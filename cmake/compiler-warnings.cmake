619d914f16085c23e1a0bfc8a368f370c62baecd
lafette21 <viragh.akos21@gmail.com>
2026-05-12 18:13:31 +0200

    Add static analysis II

--- cmake/compiler-warnings.cmake
+++ cmake/compiler-warnings.cmake
@@ -32,18 +32,13 @@ function(enable_compiler_warnings TARGET VISIBILITY)
                 # conversion has been implicitly applied
     )
 
-    set(CLANG_WARNINGS
+    set(COMMON_WARNINGS
         -Wall
         -Wextra                         # reasonable and standard
         -Wshadow                        # warn the user if a variable declaration
                                         # shadows one from a parent context
-        -Wnon-virtual-dtor              # warn the user if a class with virtual functions has a
-                                        # non-virtual destructor. This helps catch hard to
-                                        # track down memory errors
-        -Wold-style-cast                # warn for c-style casts
         -Wcast-align                    # warn for potential performance problem casts
         -Wunused                        # warn on anything being unused
-        -Woverloaded-virtual            # warn if you overload (not override) a virtual function
         -Wpedantic                      # warn if non-standard C++ is used
         -Wconversion                    # warn on type conversions that may lose data
         -Wsign-conversion               # warn on sign conversions
@@ -52,29 +47,42 @@ function(enable_compiler_warnings TARGET VISIBILITY)
         -Wformat=2                      # warn on security issues around functions that format output (ie printf)
     )
 
-    if (NOT SUPPRESS_COMPILER_WERROR)
-        set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
-        set(CLANG_WARNINGS ${CLANG_WARNINGS} -Wno-error=null-dereference)
-        set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
-    endif()
+    set(CXX_ONLY_WARNINGS
+        -Wnon-virtual-dtor              # warn the user if a class with virtual functions has a
+                                        # non-virtual destructor. This helps catch hard to
+                                        # track down memory errors
+        -Wold-style-cast                # warn for c-style casts
+        -Woverloaded-virtual            # warn if you overload (not override) a virtual function
+    )
 
-    set(GCC_WARNINGS
-        ${CLANG_WARNINGS}
+    set(GCC_EXTRA_WARNINGS
         -Wmisleading-indentation        # warn if identation implies blocks where blocks do not exist
         -Wduplicated-cond               # warn if if / else chain has duplicated conditions
         -Wduplicated-branches           # warn if if / else branches have duplicated code
         -Wlogical-op                    # warn about logical operations being used where bitwise were probably wanted
+    )
+
+    set(GCC_CXX_ONLY_WARNINGS
         -Wuseless-cast                  # warn if you perform a cast to the same type
     )
 
-    if(MSVC)
-        set(PROJECT_WARNINGS ${MSVC_WARNINGS})
-    elseif(CMAKE_CXX_COMPILER_ID MATCHES "^(Apple)?Clang$")
-        set(PROJECT_WARNINGS ${CLANG_WARNINGS})
-    else()
-        set(PROJECT_WARNINGS ${GCC_WARNINGS})
+    if(NOT SUPPRESS_COMPILER_WERROR)
+        list(APPEND COMMON_WARNINGS -Werror -Wno-error=null-dereference)
+        list(APPEND MSVC_WARNINGS /WX)
     endif()
 
-    target_compile_options(${TARGET} ${VISIBILITY} ${PROJECT_WARNINGS})
-
+    if(MSVC)
+        target_compile_options(${TARGET} ${VISIBILITY} ${MSVC_WARNINGS})
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "^(Apple)?Clang$" OR CMAKE_C_COMPILER_ID MATCHES "^(Apple)?Clang$")
+        target_compile_options(${TARGET} ${VISIBILITY}
+            ${COMMON_WARNINGS}
+            $<$<COMPILE_LANGUAGE:CXX>:${CXX_ONLY_WARNINGS}>
+        )
+    else()
+        target_compile_options(${TARGET} ${VISIBILITY}
+            ${COMMON_WARNINGS}
+            $<$<COMPILE_LANGUAGE:CXX>:${CXX_ONLY_WARNINGS} ${GCC_CXX_ONLY_WARNINGS}>
+            ${GCC_EXTRA_WARNINGS}
+        )
+    endif()
 endfunction()
